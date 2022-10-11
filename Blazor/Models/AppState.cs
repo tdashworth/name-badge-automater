cc57ea3a82621ad5ccc337c04dadaaa7f1d0290b
Tom Ashworth <tom.ashworth@capgemini.com>
2022-10-11 13:08:00 +0100

    feat: support different badge count files
    Closes #3

--- Blazor/Models/AppState.cs
+++ Blazor/Models/AppState.cs
@@ -6,6 +6,7 @@ namespace NameBadgeAutomater
 {
   public class AppState
   {
+    public int BadgesPerPage { get; set; }
     public byte[]? PowerPointFile { get; set; }
     public List<Person> People { get; set; } = new List<Person>();
     public bool HasGeneratedBadges { get; set; }
--- Blazor/Services/PowerPointTemplateService.cs
+++ Blazor/Services/PowerPointTemplateService.cs
@@ -7,36 +7,28 @@ namespace NameBadgeAutomater
 {
   public class PowerPointTemplateService
   {
-    private static readonly int BADGES_PER_PAGE = 8;
-    private static readonly string BASE_FIRSTNAME_TEMPLATE = "first_{0}";
-    private static readonly string BASE_LASTNAME_TEMPLATE = "last_{0}";
-    private static readonly List<Regex> FIRSTNAME_REGEXS = Enumerable
-      .Range(1, BADGES_PER_PAGE)
-      .Select(i => new Regex(string.Format(BASE_FIRSTNAME_TEMPLATE, i), RegexOptions.Compiled))
-      .ToList();
-    private static readonly List<Regex> LASTNAME_REGEXS = Enumerable
-      .Range(1, BADGES_PER_PAGE)
-      .Select(i => new Regex(string.Format(BASE_LASTNAME_TEMPLATE, i), RegexOptions.Compiled))
-      .ToList();
+    private static readonly Regex BASE_FIRSTNAME_TEMPLATE = new Regex("first_\\d", RegexOptions.Compiled);
+    private static readonly Regex BASE_LASTNAME_TEMPLATE = new Regex("last_\\d", RegexOptions.Compiled);
 
-    public List<string> ValidateTemplate(byte[] fileBytes)
+    public (int, List<string>) ValidateTemplate(byte[] fileBytes)
     {
       var validationErrors = new List<string>();
+      var badgesPerPage = 0;
 
       try
       {
+        // Validate File
         var presentationDocument = PresentationDocument.Open(new MemoryStream(fileBytes), true);
-
         if (presentationDocument?.PresentationPart is null)
         {
           validationErrors.Add("This file seems corrupt and cannot be read. Error: No Presentation XML part.");
-          return validationErrors; // Short circuit since all other validation will fail.
+          return (0, validationErrors); // Short circuit since all other validation will fail.
         }
 
         if (presentationDocument.PresentationPart.SlideParts.Count() == 0)
         {
           validationErrors.Add("This file has no slide. One slide is expected.");
-          return validationErrors; // Short circuit since all other validation will fail.
+          return (0, validationErrors); // Short circuit since all other validation will fail.
         }
 
         if (presentationDocument.PresentationPart.SlideParts.Count() > 1)
@@ -44,32 +36,35 @@ namespace NameBadgeAutomater
           validationErrors.Add("This file has more than one slide. One slide expected.");
         }
 
+        // Validate Slide
         using var streamReader = new StreamReader(presentationDocument.PresentationPart.SlideParts.First().GetStream());
         var slideContent = streamReader.ReadToEnd();
 
-        foreach (var regex in FIRSTNAME_REGEXS)
+        var firstNameMatches = BASE_FIRSTNAME_TEMPLATE.Matches(slideContent);
+        var lastNameMatches = BASE_LASTNAME_TEMPLATE.Matches(slideContent);
+
+        if (firstNameMatches.Count != lastNameMatches.Count)
         {
-          var matches = regex.Matches(slideContent);
-          if (matches.Count() == 0)
-          {
-            validationErrors.Add($"Could not find expected template '{regex.ToString()}.'");
-          }
-          if (matches.Count() > 1)
-          {
-            validationErrors.Add($"Multiple '{regex.ToString()} templates were found when only one was expected.'");
-          }
+          validationErrors.Add("Number of first_x and last_x templates do not match.");
         }
-        foreach (var regex in LASTNAME_REGEXS)
+
+        var firstNameMatchedTexts = firstNameMatches.Select(x => x.Value);
+        var lastNameMatchedTexts = lastNameMatches.Select(x => x.Value);
+
+        for (int index = 1; index <= firstNameMatches.Count; index++)
         {
-          var matches = regex.Matches(slideContent);
-          if (matches.Count() == 0)
-          {
-            validationErrors.Add($"Could not find expected template '{regex.ToString()}.'");
-          }
-          if (matches.Count() > 1)
-          {
-            validationErrors.Add($"Multiple '{regex.ToString()} templates were found when only one was expected.'");
-          }
+          var firstNameExpectedText = BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", index.ToString());
+          ValidateExpectedTemplateOnSlide(firstNameMatchedTexts, firstNameExpectedText, validationErrors);
+
+          var lastNameExpectedText = BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", index.ToString());
+          ValidateExpectedTemplateOnSlide(lastNameMatchedTexts, lastNameExpectedText, validationErrors);
+
+          badgesPerPage++;
+        }
+
+        if (badgesPerPage == 0)
+        {
+          validationErrors.Add("No templates were found. At least one pair of template is expected.");
         }
       }
       catch (Exception ex)
@@ -77,10 +72,10 @@ namespace NameBadgeAutomater
         validationErrors.Add($"This file seems corrupt and cannot be read. Error: {ex}");
       }
 
-      return validationErrors;
+      return (badgesPerPage, validationErrors);
     }
 
-    public byte[] GenerateFromTemplate(byte[] fileBytes, List<Person> people, bool blankUnusedBadges = true)
+    public byte[] GenerateFromTemplate(byte[] fileBytes, List<Person> people, int badgesPerPage, bool blankUnusedBadges = true)
     {
       // Open document
       var presentationDocument = PresentationDocument.Open(new MemoryStream(fileBytes), true);
@@ -91,8 +86,8 @@ namespace NameBadgeAutomater
 
       // Generate new slides 
       people
-        .Chunk(BADGES_PER_PAGE)
-        .Select(peopleGroup => GenerateNewSlideWithNames(templatePart, peopleGroup, blankUnusedBadges))
+        .Chunk(badgesPerPage)
+        .Select(peopleGroup => GenerateNewSlideWithNames(templatePart, peopleGroup, badgesPerPage, blankUnusedBadges))
         .ToList()
         .ForEach(newSlide => presentationPart.AppendSlide(newSlide));
 
@@ -106,29 +101,39 @@ namespace NameBadgeAutomater
       return outStream.ToArray();
     }
 
-    private static SlidePart GenerateNewSlideWithNames(SlidePart templatePart, Person[] peopleGroup, bool blankUnusedBadges)
+    private static void ValidateExpectedTemplateOnSlide(IEnumerable<string> foundTemplates, string expectedTemplate, List<string> validationErrors)
+    {
+      var countFound = foundTemplates.Count(x => string.Equals(x, expectedTemplate));
+      if (countFound == 0)
+      {
+        validationErrors.Add($"Could not find expected template '{expectedTemplate}.'");
+      }
+      if (countFound > 1)
+      {
+        validationErrors.Add($"Multiple '{expectedTemplate} templates were found when only one was expected.'");
+      }
+    }
+
+    private static SlidePart GenerateNewSlideWithNames(SlidePart templatePart, Person[] peopleGroup, int badgesPerPage, bool blankUnusedBadges)
     {
       var newSlidePart = templatePart.CloneSlide();
 
       for (var i = 0; i < peopleGroup.Count(); i++)
       {
         var person = peopleGroup[i];
-        var firstNameRegex = FIRSTNAME_REGEXS[i];
-        var lastNameRegex = LASTNAME_REGEXS[i];
 
-        newSlidePart.Slide.InnerXml = firstNameRegex.Replace(newSlidePart.Slide.InnerXml, person.FirstName);
-        newSlidePart.Slide.InnerXml = lastNameRegex.Replace(newSlidePart.Slide.InnerXml, person.LastName);
+        newSlidePart.Slide.InnerXml = newSlidePart.Slide.InnerXml
+          .Replace(BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), person.FirstName)
+          .Replace(BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), person.LastName);
       }
 
       if (blankUnusedBadges)
       {
-        for (var i = peopleGroup.Count(); i < BADGES_PER_PAGE; i++)
+        for (var i = peopleGroup.Count(); i < badgesPerPage; i++)
         {
-          var firstNameRegex = FIRSTNAME_REGEXS[i];
-          var lastNameRegex = LASTNAME_REGEXS[i];
-
-          newSlidePart.Slide.InnerXml = firstNameRegex.Replace(newSlidePart.Slide.InnerXml, "");
-          newSlidePart.Slide.InnerXml = lastNameRegex.Replace(newSlidePart.Slide.InnerXml, "");
+          newSlidePart.Slide.InnerXml = newSlidePart.Slide.InnerXml
+          .Replace(BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), string.Empty)
+          .Replace(BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), string.Empty);
         }
       }
 
