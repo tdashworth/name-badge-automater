6f1e5f79bca5a6e86c23dac9c4e064fd7ff924a1
Tom Ashworth <tom.ashworth@capgemini.com>
2022-10-21 10:13:45 +0100

    feat: telemetry
    commit ef6eaf3adfcfd5777f05d3510e48364617281fa3
    Author: Tom Ashworth <tom.ashworth@capgemini.com>
    Date:   Fri Oct 21 10:12:13 2022 +0100
    
        feat: add analytics toast mesage
    
    commit ccf87313ba4fc2d1403d3e63dfc5d0a98ca50ab5
    Author: Tom Ashworth <tom.ashworth@capgemini.com>
    Date:   Thu Oct 20 17:39:54 2022 +0100
    
        feat: local storage disabler
    
    commit 2cb81d512273d2006899e1b7fb3b5073a0f0bace
    Author: Tom Ashworth <tom.ashworth@capgemini.com>
    Date:   Thu Oct 20 17:33:12 2022 +0100
    
        feat: add telemetry

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,3 +1,5 @@
+using BlazorApplicationInsights;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NameBadgeAutomater;
@@ -6,9 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton(new AppState());
-builder.Services.AddSingleton(new PowerPointTemplateService());
+builder.Services.AddSingleton<AppState>();
+builder.Services.AddSingleton<PowerPointTemplateService>();
 
 builder.Services.AddBeforeUnload();
+builder.Services.AddBlazorApplicationInsights();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
