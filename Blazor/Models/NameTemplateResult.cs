57490e25c49db2aeb052b298d565951d7590a79d
Tom Ashworth <tom.ashworth@capgemini.com>
2022-10-31 19:22:49 +0000

    wip: advanced template validation

---
+++ Blazor/Models/NameTemplateResult.cs
@@ -0,0 +1,13 @@
+
+using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace NameBadgeAutomater
+{
+  public class NameTemplateResult
+  {
+    public int Index {get; set; }
+    public int FirstCount { get; set; }
+    public int LastCount { get; set; }
+  }
+}
--- Blazor/Services/PowerPointTemplateService.cs
+++ Blazor/Services/PowerPointTemplateService.cs
@@ -7,73 +7,73 @@ namespace NameBadgeAutomater
 {
   public class PowerPointTemplateService
   {
-    private static readonly Regex BASE_FIRSTNAME_TEMPLATE = new Regex("first_\\d", RegexOptions.Compiled);
-    private static readonly Regex BASE_LASTNAME_TEMPLATE = new Regex("last_\\d", RegexOptions.Compiled);
+    private static readonly Regex TEMPLATE_REGEX = new Regex("(first|last)_\\d+", RegexOptions.Compiled);
 
-    public (int, List<string>) ValidateTemplate(byte[] fileBytes)
+    public IEnumerable<NameTemplateResult> DiscoverTemplates(byte[] fileBytes)
     {
-      var validationErrors = new List<string>();
-      var badgesPerPage = 0;
+      PresentationDocument presentationDocument = null!;
 
       try
       {
-        // Validate File
-        var presentationDocument = PresentationDocument.Open(new MemoryStream(fileBytes), true);
-        if (presentationDocument?.PresentationPart is null)
-        {
-          validationErrors.Add("This file seems corrupt and cannot be read. Error: No Presentation XML part.");
-          return (0, validationErrors); // Short circuit since all other validation will fail.
-        }
-
-        if (presentationDocument.PresentationPart.SlideParts.Count() == 0)
-        {
-          validationErrors.Add("This file has no slide. One slide is expected.");
-          return (0, validationErrors); // Short circuit since all other validation will fail.
-        }
-
-        if (presentationDocument.PresentationPart.SlideParts.Count() > 1)
-        {
-          validationErrors.Add("This file has more than one slide. One slide expected.");
-        }
-
-        // Validate Slide
-        using var streamReader = new StreamReader(presentationDocument.PresentationPart.SlideParts.First().GetStream());
-        var slideContent = streamReader.ReadToEnd();
-
-        var firstNameMatches = BASE_FIRSTNAME_TEMPLATE.Matches(slideContent);
-        var lastNameMatches = BASE_LASTNAME_TEMPLATE.Matches(slideContent);
-
-        if (firstNameMatches.Count != lastNameMatches.Count)
-        {
-          validationErrors.Add("Number of first_x and last_x templates do not match.");
-        }
-
-        var firstNameMatchedTexts = firstNameMatches.Select(x => x.Value);
-        var lastNameMatchedTexts = lastNameMatches.Select(x => x.Value);
-
-        for (int index = 1; index <= firstNameMatches.Count; index++)
-        {
-          var firstNameExpectedText = BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", index.ToString());
-          ValidateExpectedTemplateOnSlide(firstNameMatchedTexts, firstNameExpectedText, validationErrors);
-
-          var lastNameExpectedText = BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", index.ToString());
-          ValidateExpectedTemplateOnSlide(lastNameMatchedTexts, lastNameExpectedText, validationErrors);
-
-          badgesPerPage++;
-        }
-
-        if (badgesPerPage == 0)
-        {
-          validationErrors.Add("No templates were found. At least one pair of template is expected.");
-        }
+        presentationDocument = PresentationDocument.Open(new MemoryStream(fileBytes), true);
       }
       catch (Exception ex)
       {
-        validationErrors.Add($"This file seems corrupt and cannot be read. Error: {ex}");
+        throw new Exception($"This file seems corrupt and cannot be read. Error: {ex}");
       }
 
-      return (badgesPerPage, validationErrors);
+      if (presentationDocument?.PresentationPart is null)
+      {
+        throw new Exception("This file seems corrupt and cannot be read. Error: No Presentation XML part.");
+      }
+
+      if (presentationDocument.PresentationPart.SlideParts.Count() == 0)
+      {
+        throw new Exception("This file has no slide. One slide is expected.");
+      }
+
+      if (presentationDocument.PresentationPart.SlideParts.Count() > 1)
+      {
+        throw new Exception("This file has more than one slide. One slide expected.");
+      }
+
+      // Validate Slide
+      using var streamReader = new StreamReader(presentationDocument.PresentationPart.SlideParts.First().GetStream());
+      var slideContent = streamReader.ReadToEnd();
+
+      var matchedTemplateTexts = TEMPLATE_REGEX
+                                  .Matches(slideContent)
+                                  .Select(x => x.Value)
+                                  .ToList();
+
+      var groupedTemplates = matchedTemplateTexts
+                              .GroupBy(x => x)
+                              .ToDictionary(x => x.Key, x => x.Count());
+
+      var indexedTemplates = groupedTemplates
+                              .GroupBy(x => x.Key.Split("_").Last())
+                              .Select(x => new NameTemplateResult
+                              {
+                                Index = int.Parse(x.Key),
+                                FirstCount = x.FirstOrDefault(xx => xx.Key.StartsWith("first")).Value,
+                                LastCount = x.FirstOrDefault(xx => xx.Key.StartsWith("last")).Value,
+                              })
+                              .ToList();
+
+      // Add missing indexes.
+      for (int index = 1; index <= indexedTemplates.Max(x => x.Index); index++)
+      {
+        if (indexedTemplates.SingleOrDefault(x => x.Index == index) is not null) continue;
+
+        indexedTemplates.Add(new NameTemplateResult { Index = index });
+      }
+
+      return indexedTemplates;
     }
+
+    public bool IsTemplateValid(IEnumerable<NameTemplateResult> discoveredTemplates) => 
+      discoveredTemplates.Count() > 0 &&
+      discoveredTemplates.All(x => x.FirstCount > 0 && x.LastCount > 0);
 
     public byte[] GenerateFromTemplate(byte[] fileBytes, List<Person> people, int badgesPerPage, bool blankUnusedBadges = true)
     {
@@ -101,19 +101,6 @@ namespace NameBadgeAutomater
       return outStream.ToArray();
     }
 
-    private static void ValidateExpectedTemplateOnSlide(IEnumerable<string> foundTemplates, string expectedTemplate, List<string> validationErrors)
-    {
-      var countFound = foundTemplates.Count(x => string.Equals(x, expectedTemplate));
-      if (countFound == 0)
-      {
-        validationErrors.Add($"Could not find expected template '{expectedTemplate}.'");
-      }
-      if (countFound > 1)
-      {
-        validationErrors.Add($"Multiple '{expectedTemplate} templates were found when only one was expected.'");
-      }
-    }
-
     private static SlidePart GenerateNewSlideWithNames(SlidePart templatePart, Person[] peopleGroup, int badgesPerPage, bool blankUnusedBadges)
     {
       var newSlidePart = templatePart.CloneSlide();
@@ -123,8 +110,8 @@ namespace NameBadgeAutomater
         var person = peopleGroup[i];
 
         newSlidePart.Slide.InnerXml = newSlidePart.Slide.InnerXml
-          .Replace(BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), person.FirstName)
-          .Replace(BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), person.LastName);
+          .Replace("first_".ToString().Replace("\\d", (i + 1).ToString()), person.FirstName)
+          .Replace("last_".ToString().Replace("\\d", (i + 1).ToString()), person.LastName);
       }
 
       if (blankUnusedBadges)
@@ -132,8 +119,8 @@ namespace NameBadgeAutomater
         for (var i = peopleGroup.Count(); i < badgesPerPage; i++)
         {
           newSlidePart.Slide.InnerXml = newSlidePart.Slide.InnerXml
-          .Replace(BASE_FIRSTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), string.Empty)
-          .Replace(BASE_LASTNAME_TEMPLATE.ToString().Replace("\\d", (i+1).ToString()), string.Empty);
+          .Replace("first_".ToString().Replace("\\d", (i + 1).ToString()), string.Empty)
+          .Replace("last_".ToString().Replace("\\d", (i + 1).ToString()), string.Empty);
         }
       }
 
