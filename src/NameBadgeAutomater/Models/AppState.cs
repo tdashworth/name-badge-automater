8fce4a1c2bdfa79ff6bf2e358bfe02f1e89d9681
Tom Ashworth <tom.ashworth@capgemini.com>
2022-11-18 15:05:09 +0000

    refactor: move discovered placeholders and file name to app state

--- src/NameBadgeAutomater/Models/AppState.cs
+++ src/NameBadgeAutomater/Models/AppState.cs
@@ -6,10 +6,17 @@ namespace NameBadgeAutomater
 {
   public class AppState
   {
+    // Upload File
     public byte[]? PowerPointFile { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public IEnumerable<NameTemplateResult>? DiscoveredPlaceholders { get; set; }
     public int BadgesPerPage { get; set; }
+
+    // Enter Names
     public string RawNames { get; set; } = string.Empty;
     public List<Person> People { get; set; } = new List<Person>();
+
+    // Generate
     public bool HasGeneratedBadges { get; set; }
   }
 }
