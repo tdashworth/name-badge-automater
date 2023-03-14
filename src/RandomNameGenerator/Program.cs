022bdcf8dbb5255e160e7b2416deecc80c098f20
Tom Ashworth <tom.ashworth@capgemini.com>
2023-03-14 17:29:05 +0000

    feat: name generator

---
+++ src/RandomNameGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿var names = Enumerable.Range(0, 50)
+    //.Select(_ => $"{Faker.Name.First()} {Faker.Name.Last()}");
+    // .Select(_ => $"{Faker.Name.First()} {Faker.Name.Middle()} {Faker.Name.Last()}");
+    // .Select(_ => $"{Faker.Name.Middle()} {Faker.Name.Last()}, {Faker.Name.First()}");
+    // .Select(_ => $"{Faker.Name.First().ToLower()}.{Faker.Name.Last().ToLower()}@{Faker.Internet.DomainName()}");
+    // .Select(_ =>
+    // {
+    //   var first = Faker.Name.First();
+    //   var last = Faker.Name.Last();
+
+    //   return $"{first} {last} <{first.ToLower()}.{last.ToLower()}@{Faker.Internet.DomainName()}>";
+    // });
+    // .Select(_ =>
+    //  {
+    //    var first = Faker.Name.First();
+    //    var last = Faker.Name.Last();
+
+    //    return $"{last}, {first} <{first.ToLower()}.{last.ToLower()}@{Faker.Internet.DomainName()}>";
+    //  })
+     .Select(_ =>
+     {
+       var first = Faker.Name.First();
+       var last = Faker.Name.Last();
+
+       return $"{Faker.Name.Middle()} {last}, {first} <{first.ToLower()}.{last.ToLower()}@{Faker.Internet.DomainName()}>";
+     })
+    //  .OrderBy(_ => Faker.RandomNumber.Next())
+     ;
+// .Select(_ => $"{Faker.Name.First()} {Faker.Name.Last()}");
+// .Select(_ => $"{Faker.Name.First()} {Faker.Name.Last()}");
+
+foreach (var name in names)
+{
+  Console.WriteLine(name);
+}
