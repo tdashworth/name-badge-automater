f14f2ad2f8931be9435bcb97a995f46f0a87f0d6
Tom Ashworth <thomas.d.ashworth@gmail.com>
2023-03-26 20:57:18 +0100

    docs: love letter 💖

--- src/NameBadgeAutomater/Program.cs
+++ src/NameBadgeAutomater/Program.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NameBadgeAutomater;
 
+// I love you, more than I can put into words, more than I even know.
+// All I can do is write code and hope it can make the world a better place.
+// ...and put a smile on your face everytime you execute it. 
+// Your smile brightens my whole world, right as I sit across the table writing this. 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
